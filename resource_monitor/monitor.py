d5a68639c354f9b1d8071d8244241530f2766ffb
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-08-06 14:52:26 +0000

    Add site paths for raw data

--- resource_monitor/monitor.py
+++ resource_monitor/monitor.py
@@ -11,29 +11,55 @@ disk_quota_cmd = ["quota", "-vs", "-p", "-w"]
 
 app = Flask('app', template_folder="resource_monitor/template/")
 
-def get_cpu():
+def get_stats_by_pid():
     stats: dict = {}
     for pid in psutil.pids():
         process = psutil.Process(pid)
-        stats[pid] = {
-            "name": p.name(),
+        stats[f"{pid}"] = {
+            "name": process.name(),
             "cpu": process.cpu_percent(),
-            "memory": process.memory_info().rss
+            "memory": process.memory_info().rss,
+            "status": process.status()
         }
     return stats
 
 
 @app.route('/')
 def index():
-    print(get_cpu())
     return flask.render_template("monitor.html")
 
-@app.route('/cpu')
-def cpu():
-    return str(psutil.cpu_percent())
+@app.route('/data/by_pid')
+def data_pid():
+    return get_stats_by_pid()
 
-@app.route('/memory')
-def memory():
-    return str(psutil.virtual_memory())
+@app.route('/data/global')
+def data_global():
+    memory = psutil.virtual_memory()
+    storage = psutil.disk_usage('/')
+    data = {
+        "cpu": {
+            "usage": psutil.cpu_percent(),
+            "frequency": psutil.cpu_freq(),
+            "per-core": psutil.cpu_percent(percpu=True)
+        },
+        "memory": {
+            "free": memory.available,
+            "used": memory.used,
+            "total": memory.total,
+            "percent": memory.percent
+        },
+        "storage": {
+            "total": storage.total,
+            "used": storage.used,
+            "free": storage.free,
+            "percent": storage.percent
+        }
+    }
+    return data
+
+"""
+psutil.cpu_percent()
+psutil.virtual_memory()
+"""
 
 app.run(host='0.0.0.0', port=8080)
