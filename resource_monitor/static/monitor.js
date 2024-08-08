feab9bf480acacbda6f02012efae19b1c8b9f009
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-08-08 14:20:16 +0000

    Continue charting

--- resource_monitor/static/monitor.js
+++ resource_monitor/static/monitor.js
@@ -1,14 +1,44 @@
 const globalGraph = new Chart("cpu-graph", {
-    type: "line",
+    type: 'line',
     data: {
-        labels: ["1", "2", "3"],
+        labels: ['12:45:40', '12:45:41', '12:45:42', '12:45:43'],
         datasets: [{
-            backgroundColor:"rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
-            data: ["60%", "75%", "90%"]
+            label: 'My Usage',
+            data: [10, 11.5, 14, 13.87],
+            hoverBorderWidth: 1,
+            hoverBorderColor: 'rgba(0, 0, 0, 0.1)',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)'
+        }, {
+            label: 'Global Usage',
+            data: [50.67, 53.89, 53.63, 48.79],
+            hoverBorderWidth: 1,
+            hoverBorderColor: 'rgba(0, 0, 0, 0.1)',
+            backgroundColor: 'rgba(201, 203, 207, 0.2)',
+            borderColor: 'rgba(201, 203, 207, 1)'
         }]
     },
-    options: {}
+    options: {
+        legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+                usePointStyle: true,
+                boxWidth: 6,
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 100,
+                    callback: function(value) {
+                        return value + '%';
+                    }
+                }
+            }]
+        }
+    }
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
