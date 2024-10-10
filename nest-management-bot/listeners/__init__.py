cbdf72d740248554aebc404051a7c6c1ce4b23c3
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-10-10 15:25:20 +0000

    Setup the bot outline a bit

---
+++ nest-management-bot/listeners/__init__.py
@@ -0,0 +1,8 @@
+# Template from slack, jst leaving those in so i remember what im meant to do
+
+#from listeners import actions
+
+
+def register_listeners(app):
+    ...
+    #actions.register(app)
--- nest-management-bot/main.py
+++ nest-management-bot/main.py
@@ -1,1 +1,19 @@
-# WIP - main bot
+import os
+import logging
+
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from listeners import register_listeners
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Initialization
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Register Listeners
+register_listeners(app)
+
+# Start Bolt app
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
