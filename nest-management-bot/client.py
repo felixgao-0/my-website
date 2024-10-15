32e4cc44608c3a1a68d01bb86c6efad0c78257fb
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-10-14 21:45:04 -0400

    Start working on data plumbing (wip)

--- nest-management-bot/client.py
+++ nest-management-bot/client.py
@@ -1,14 +1,43 @@
+import atexit
 import asyncio
 import json
+import os
 import ssl
-import pathlib
+import subprocess
 
-import aioconsole
-import websockets
+import psutil
 from websockets.asyncio.client import connect
 
 
 __version__ = "0.1.0a" # Use for later version checking
+
+
+status_emojis = {
+    "running": "🏃",
+    "sleeping": "😴",
+    "zombie": "🧟",
+    "stopped": "⏹️",
+    "disk-sleep": "💽"
+}
+
+
+def get_storage() -> list:
+    """
+    Get the storage used by the user
+    :return: Files, folders, etc. in a list
+    """
+    result = subprocess.run(
+        ["du", "--max-depth=1", "-c", "-b", os.getcwd()],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    new_result = []
+    for file_path in result.stdout.splitlines():
+        split = file_path.split("\t")
+        new_result.append(split)
+
+    return new_result
 
 
 async def client():
@@ -20,7 +49,16 @@ async def client():
     print("Attempting to connect to server...")
     async for websocket in connect(uri):#, ssl=ssl_context):
         print("Connected to server...")
-        await websocket.send(json.dumps({'version': __version__}))
+        #atexit.register(await websocket.close()) # Register exit handler
+
+        await websocket.send(json.dumps(
+            {
+                'status': "let_me_in_pls",
+                'payload': {
+                    'version': __version__
+                }
+            }
+        ))
 
         async for message_json in websocket:
             try:
@@ -31,17 +69,89 @@ async def client():
             msg_status = message.get('status')
             msg_data = message.get('message')
             if msg_status == 'command':
-                msg_data = msg_data
-                print(f'COMMAND: {msg_data}')
-                await websocket.send(json.dumps({'status': 'command_response', 'message': 'i downloaded it already, let my family go!'}))
-
+                websocket.send(json.dumps(command_handler(msg_status, msg_data.get('payload'))))
             elif msg_status == 'error':
                 print(f'ERROR: {msg_data}')
-                ...
             elif msg_status == 'info':
                 print(f'MESSAGE: {msg_data}')
             else:
-                ... # idk
+                print(f'Message of unknown "{msg_status}" type: {msg_data}')
+
+
+def command_handler(status: str, payload: dict) -> dict:
+    if status == "obtain_global_info":
+        """
+        Provides global system information for all of Nest
+        """
+        memory = psutil.virtual_memory()
+        storage = psutil.disk_usage('/')
+
+        return {
+            "status": "command_response",
+            "payload": {
+                "cpu": {
+                    "usage": psutil.cpu_percent(interval=0.1),
+                    "frequency": psutil.cpu_freq().current
+                },
+                "memory": {
+                    "total": memory.total,
+                    "used": memory.used
+                },
+                "storage": {
+                    "total": storage.total,
+                    "used": storage.used
+                }
+            }
+        }
+
+    elif status == "obtain_all_process_info":
+        """
+        Lists running processes and associated systemd data if it exists 
+        """
+        process_data = []
+        for process in psutil.process_iter():
+            process_data.append({
+                "pid": process.pid,
+                "name": process.name(),
+            })
+
+        return process_data
+
+    elif status == "obtain_process_info":
+        """
+        Provides information about a specific process
+        """
+        # Get process
+        process = psutil.Process(payload.get('pid'))
+        return {
+            "status": "command_response",
+            "payload": {
+                "pid": process.pid,
+                "name": process.name(),
+                "cpu_usage": process.cpu_percent(),
+                "cpu_time": {
+                    "user": process.cpu_times().user,
+                    "system": process.cpu_times().system
+                },
+                "status": process.status()
+            }
+        }
+
+    elif status == "kill_process":
+        process = psutil.Process(payload.get('pid'))
+        try:
+            process.kill()
+        except:
+            return {
+                "status": "error",
+                "message": "Process was not killed"
+            }
+    elif status == "restart_process":
+        ...
+    elif status == "start_process":
+        ...
+    elif status == "exec_command":
+        ... # uhhh
 
 
 
