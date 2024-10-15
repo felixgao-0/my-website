d2820bf8f93b317558712df059bc879a6a044fb6
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-10-14 21:44:39 -0400

    Replace utils.py with server_utils.py. Move functions from server.py to server_utils.py

--- nest-management-bot/server.py
+++ nest-management-bot/server.py
@@ -1,33 +1,12 @@
 import asyncio
 import json
 import ssl
-import socket
-from typing import Literal, Optional
 
 from websockets.asyncio.server import serve
 import websockets
 
-# Maintain a list of connected clients
-clients: dict = {}
-
-
-async def send_error(websocket, error_msg, *, possible=True, disconnect=True) -> None:
-    """
-    Formats an error message in json to be sent to the client.
-    :param websocket: Websocket instance to use
-    :param error_msg: The message to be formatted
-    :param possible: If this error is possible with a correct client
-    :param disconnect: Disconnect the server after the error message is sent
-    :return:
-    """
-    await websocket.send(json.dumps(
-            {
-                'message': f'{error_msg}{' This shouldn\'t happen, send a message in #slack-management-bot for help.' if not possible else ''}',
-                'status': 'error'
-            }
-        ))
-    if disconnect:
-        await websocket.close()
+import server_utils as utils
+from server_utils import clients
 
 
 async def server(websocket):
@@ -36,19 +15,24 @@ async def server(websocket):
         try:
             first_message_json = await asyncio.wait_for(websocket.recv(), timeout=60)
         except asyncio.TimeoutError:
-            await send_error(websocket, 'Authentication message was not received.', possible=False)
+            await utils.send_error(websocket, 'Authentication message was not received.', possible=False)
             return
 
         try:
             first_message = json.loads(first_message_json)
         except json.decoder.JSONDecodeError:
-            await send_error(websocket, 'Authentication message isn\'t json.', possible=False)
+            await utils.send_error(websocket, 'Authentication message isn\'t json.', possible=False)
             return
 
-        if first_message.get('version') != "0.1.0a":
-            await send_error(websocket, 'Invalid client version or malformed data.', possible=False)
+        if first_message.get('status') != "let_me_in_pls":
+            await utils.send_error(websocket, 'Invalid response type.', possible=False)
             return
 
+        elif first_message.get('payload').get('version') != "0.1.0a":
+            await utils.send_error(websocket, 'Invalid client version or malformed data.', possible=False)
+            return
+
+        # User authenticated yay
         await websocket.send(json.dumps({'status': 'info', 'message': 'Authenticated :D'}))
 
         # Add client to list to use for sending messages later
@@ -56,20 +40,8 @@ async def server(websocket):
         print("ACTIVE CLIENTS:", clients.keys())
 
 
-        try:
-            cmd_response = await asyncio.wait_for(
-                send_message('download raid shadow legends!', f'{websocket.id}', message_type='command'),
-                5
-            )
-        except asyncio.TimeoutError:
-            await send_error(websocket, 'Did not receive a response in time', possible=False)
-            return
-
-        print(f"RESPONSE: {cmd_response}")
-
-
         async for message in websocket:
-            print(f"Received message: {message}")
+            print(f"RANDOM MESSAGE: {message}")
             if message == "close":
                 await websocket.close()
                 return
@@ -79,45 +51,7 @@ async def server(websocket):
     finally:
         if clients.get(websocket.id):
             clients.pop(f'{websocket.id}')
-            print("ACTIVE CLIENTS:", clients.keys())
-        else:
-            print('Client was not added to dict :/')
-
-
-async def send_message(message: str, user_uuid: str, *, message_type: Literal['info', 'command']) -> Optional[dict]:
-    """
-    Sends a message of either "info" or "command" type
-    :param message: The message to send
-    :param user_uuid: The client to send a message to
-    :param message_type: The type of message, a response will be returned if the type is "command"
-    :return:
-    """
-    await clients[user_uuid].send(json.dumps({'status': message_type, 'message': message}))
-    response_json = await clients[f'{user_uuid}'].recv()
-
-    if message_type == 'command':
-        try:
-            response = json.loads(response_json)
-        except json.decoder.JSONDecodeError:
-            await send_error(await clients[f'{user_uuid}'], 'Invalid Response', possible=False)
-            return
-
-        if response.get('status') == 'command_response':
-            return response
-        else:
-            await send_error(await clients[f'{user_uuid}'], 'Invalid Response type', possible=False)
-
-
-def ident_request(local_port, remote_port):
-    try:
-        with socket.create_connection(('localhost', 113), timeout=10) as sock:
-            query = f"{local_port}, {remote_port}\r\n"
-            sock.sendall(query.encode())
-
-            response = sock.recv(1024).decode().strip()
-            return response
-    except Exception as e:
-        return f"Error: {e}"
+        print("ACTIVE CLIENTS:", clients.keys())
 
 
 async def main():
@@ -128,6 +62,19 @@ async def main():
         print("server running...")
         await asyncio.get_running_loop().create_future()  # run forever
 
+
+"""
+try:
+    cmd_response = await asyncio.wait_for(
+        utils.send_command('download raid shadow legends!', f'{websocket.id}'),
+        5
+    )
+except asyncio.TimeoutError:
+    await utils.send_error(websocket, 'Did not receive a response in time', possible=False)
+    return
+
+print(f"RESPONSE: {cmd_response}")
+"""
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ nest-management-bot/server_utils.py
@@ -0,0 +1,106 @@
+"""
+All the utilities used by server.py to make life easier lol
+"""
+import hashlib
+import secrets
+import json
+import socket
+from typing import Optional
+
+clients = {}
+
+
+class ClientError(Exception):
+    pass
+
+
+async def send_message(message: str, user_uuid: str) -> None:
+    """
+    Send a message to the client
+    :param message: The message to send
+    :param user_uuid: The client to send a message to
+    :return:
+    """
+    await clients[user_uuid].send(json.dumps({'status': 'info', 'message': message}))
+
+
+async def send_command(message: str, user_uuid: str) -> Optional[dict]:
+    """
+    Send a command to the client
+    :param message: The message to send
+    :param user_uuid: The client to send a message to
+    :return: The client's response to the command
+    """
+    await clients[user_uuid].send(json.dumps({'status': 'command', 'message': message}))
+    response_json = await clients[user_uuid].recv()
+
+    try:
+        response = json.loads(response_json)
+    except json.decoder.JSONDecodeError:
+        await send_error(await clients[f'{user_uuid}'], 'Invalid Response', possible=False)
+        return
+
+    if response.get('status') == 'command_response':
+        return response
+    elif response.get('status') == 'error':
+        raise ClientError(response.get('message'))
+    else:
+        await send_error(await clients[f'{user_uuid}'], 'Invalid Response type', possible=False)
+
+
+async def send_error(error_msg, user_uuid: str, *, possible=False, disconnect=True) -> None:
+    """
+    Send an error to the client
+    :param error_msg: The error to send
+    :param user_uuid: The client to send a message to
+    :param possible: If this error is possible with a correct client
+    :param disconnect: Disconnect the server after the error message is sent
+    :return:
+    """
+    await clients[user_uuid].send(json.dumps(
+        {
+            'message': f'{error_msg}{' This shouldn\'t happen, send a message in #slack-management-bot for help.' if not possible else ''}',
+            'status': 'error'
+        }
+    ))
+    if disconnect:
+        await clients[user_uuid].close()
+
+
+
+def ident_request(local_port, remote_port) -> str:
+    """
+    Send an ident request to identify the user from a websocket port
+    :param local_port:
+    :param remote_port:
+    :return: Username of websocket client
+    """
+    with socket.create_connection(('localhost', 113), timeout=10) as sock:
+        query = f"{local_port}, {remote_port}\r\n"
+        sock.sendall(query.encode())
+
+        response = sock.recv(1024).decode().strip()
+        return response
+
+
+# Generate_token() and verify_token_checksum() might not end up being used, but im saving the code snippets for now
+
+def generate_token() -> str:
+    raw_token = secrets.token_hex(32)
+
+    checksum = hashlib.sha256(raw_token.encode()).hexdigest()[:8]
+    opaque_token = f"{raw_token}.{checksum}"
+
+    return opaque_token
+
+
+def verify_token_checksum(token: str) -> bool:
+    if len(token) != 73: # Token must be 73 chars long
+        return False
+
+    raw_token = token[:-9] # Obtain raw token, minus the seperator
+    checksum = token[-8:] # Obtain checksum
+
+    expected_checksum = hashlib.sha256(raw_token.encode()).hexdigest()[:8]
+
+    return expected_checksum == checksum
