cea041aa3dc0a764a96563ee53b784a07bc0ba48
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-10-16 19:10:06 +0000

    Add database code, setup parts of user onboarding (wip)

--- nest-management-bot/main.py
+++ nest-management-bot/main.py
@@ -1,3 +1,4 @@
+import atexit
 import os
 import logging
 
@@ -13,27 +14,51 @@ import certifi
 import database
 import views.home as home
 
+import server_utils as utils
+
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-# Initialize your app with your bot token and signing secret
+# Initialize bot with token and signing secret
 app = App(
     token=os.environ["SLACK_MANAGEMENT_BOT_TOKEN"],
     signing_secret=os.environ["SLACK_MANAGEMENT_SIGNING_SECRET"]
 )
 
+# Open database for account management
+db = database.Database({
+    "dbname": "felixgao_nest_management",
+    "user": "felixgao",
+    "password": os.environ['DB_PASSWORD'],
+    "host": "hackclub.app",
+    "port": "5432"
+    })
+
 me = os.environ['MY_SLACK_ID']
+
 
 @app.event("app_home_opened")
 def update_home_tab(client, event):
-    if event['user'] != me:
+    user_id = event['user']
+    if user_id != me: 
+        # Testing check, blocks others from using D:
         home.generate_unauthorized(client, event)
 
-    #if event['user']
-    #home.generate_unauthorized(client, event)
+    if not db.get_user(slack_id=user_id): 
+        #User not registered, signup!
+        home.generate_setup(client, event)
 
+
+# TODO: Action btn to generate code
+def setup_user(client):
+    user_token = utils.generate_token()
+    #db.add_user("U07BU2HS17Z", user_token)
+
+
+# Close the database on code end
+atexit.register(lambda: db.close())
 
 if __name__ == "__main__":
     app.start(port=8000)
