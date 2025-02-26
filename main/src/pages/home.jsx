1172560c8ae0e30b99adf0f0a414d37dc4cffcfe
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2025-02-26 09:18:45 -0500

    more setup

---
+++ main/src/pages/home.jsx
@@ -0,0 +1,25 @@
+function Home() {
+    return (
+        <>
+        <div class='h-screen px-10 py-10 flex flex-col justify-between'>
+            <img class="w-full h-auto object-cover" src='/cs_ottawa.png' alt='A photo of counterspell ottawa'></img>
+            <div class='apx-10 py-10 flex flex-col justify-between'>
+            <h2 class='text-4xl text-white'>Hello! 👋</h2>
+            <h1 class='text-5xl text-white'>I’m Felix, a <span class='text-amber-400'>Hack Clubber</span>.</h1>
+            </div>
+
+            <div>
+            <p class='text-3xl text-white'>Welcome to my chunk of the internet :D.</p>
+            <p class='text-white'>Pictured: Counterspell Ottawa, a hackathon I attended in November of 2024 :D</p>
+            </div>
+        </div>
+        <div class='space-y-4 mx-10 pt-5'>
+            <p>I’m Felix (he/him)! A teen living in Ontario, I (try to) build cool things in my free time whilst drowning in school work. I have an interesting variety of niche interests and I love building software to do cool things. I believe that technology has the power to change the world for the better, and I want to be part of that magic! My goal is to build things people will use to improve their lives and make the world better. In my limited free time you can find me playing video games, watching YouTube videos on niche topics, and bashing (bash pun intended) my head against keyboards (/ex) while writing code.</p>
+            <p>I love urban design, the process of designing our cities and communities! I think its a core part of how we live our lives. I believe that public transit, cycling and walking are the most efficient and sustainable forms of transit, and we’re too dependent on cars and build a society as such.</p>
+            <p>I enjoy learning about politics and how people view our world. I have a friend who asks me crazy philosophical questions, and they often bring about interesting conversations.</p>
+        </div>
+        </>
+    );
+  }
+  
+export default Home;
