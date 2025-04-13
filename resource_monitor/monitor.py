7e513b2933706bcf44f7a662c3fea588e566ab08
Maher <114317937+thatonecodes@users.noreply.github.com>
2025-04-12 22:46:27 -0400

    feat: fix bugs and add visual indicator in qr code
    - fixed spamming requests by adding `Flask-Limiter` and limiting
      requests to `3/1s`
    - added function `check_redis_connection` to
      `resource_monitor/monitor.py` for checking if a redis connection is
      open on `localhost:6379` if ran manually
    - added visual indicator in QR code to know what image is selected
    - changed `resource_monitor.service` file to ensure `redis.service` is
      active before running + added absolute path to python3 `/usr/bin/python3`
    - updated `requirements.txt` to include updated pkgs
    - modified info in `README.md` to include redis setup guide

--- resource_monitor/monitor.py
+++ resource_monitor/monitor.py
@@ -2,30 +2,40 @@
 Nest Resouce Monitor - Backend: This runs a flask server to run the site's backend with psutil
 """
 
-import subprocess
 import os
+import subprocess
 
-import psutil
-
-from flask import Flask
 import flask
-
+import psutil
+import redis
 from dotenv import load_dotenv
+from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
-#import global_utils
+# import global_utils
 
-#from flask_socketio import SocketIO
+# from flask_socketio import SocketIO
 
 # Load my .env file :)
 load_dotenv()
 
 app = Flask(
-    'app', 
+    "app",
     template_folder="resource_monitor/template/",
-    static_folder='resource_monitor/static'
+    static_folder="resource_monitor/static",
 )
-#app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
-#socketio = SocketIO(app)
+# app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
+# socketio = SocketIO(app)
+
+# Limit the requests to prevent DDoS from the inside
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri="redis://localhost:6379",
+    storage_options={"socket_connect_timeout": 30},
+    strategy="fixed-window",
+)
 
 # Add emojis cause they look nice
 status_emojis = {
@@ -33,27 +43,53 @@ status_emojis = {
     "sleeping": "😴",
     "zombie": "🧟",
     "stopped": "⏹️",
-    "disk-sleep": "💽"
+    "disk-sleep": "💽",
 }
+
 
 def get_storage():
     result = subprocess.run(
         ["du", "--max-depth=1", "-c", "-b", os.getcwd()],
         capture_output=True,
         text=True,
-        check=True
+        check=True,
     )
     new_result = []
     for file_path in result.stdout.splitlines():
         split = file_path.split("\t")
-        new_result.append(split) # Add item to dict
+        new_result.append(split)  # Add item to dict
 
     return new_result
 
 
-@app.route('/')
+def check_redis_connection() -> bool:
+    """
+    This function checks if redis is connected, using the redis module.
+
+    Returns:
+        `bool` - if redis server is running on port 6379 or not.
+    """
+    try:
+        r = redis.Redis(host="localhost", port=6379)
+        r.ping()
+        return True
+    except redis.ConnectionError as e:
+        print(f"Redis connection error: {e}")
+        print("Please start redis and try again.")
+        return False
+    except redis.TimeoutError as e:
+        print(f"Redis timeout error: {e}")
+        print("Connection to Redis timed out.")
+        return False
+    except Exception as e:
+        print(f"Unexpected error connecting to Redis: {e}")
+        return False
+
+
+@app.route("/")
 def index():
     return flask.render_template("monitor.html")
+
 
 """
 @app.route("/run_commands")
@@ -77,50 +113,68 @@ def get_cmd_data():
     }
 """
 
-@app.route('/data')
+
+@app.route("/data")
+@limiter.limit("3 per second")
 def data_pid():
     memory = psutil.virtual_memory()
-    storage = psutil.disk_usage('/')
+    storage = psutil.disk_usage("/")
+    total_mem = 0
+    total_cpu = 0
 
-    stats: dict = {
-        "by_pid": [], 
-        "total": {
-            "cpu": {
-                "usage": psutil.cpu_percent(interval=0.1),
-                "frequency": psutil.cpu_freq().current,
-                "per-core": psutil.cpu_percent(interval=0.1, percpu=True)
+    try:
+        stats: dict = {
+            "by_pid": [],
+            "total": {
+                "cpu": {
+                    "usage": psutil.cpu_percent(interval=0.1),
+                    "frequency": psutil.cpu_freq().current,
+                    "per-core": psutil.cpu_percent(interval=0.1, percpu=True),
+                },
+                "memory": {
+                    "total": memory.total,
+                    "used": memory.used,
+                    # "free": memory.available,
+                    "percent": memory.percent,
+                },
+                "storage": {
+                    "total": storage.total,
+                    "used": storage.used,
+                    "free": storage.free,
+                    "percent": storage.percent,
+                },
             },
-            "memory": {
-                "total": memory.total,
-                "used": memory.used,
-                #"free": memory.available,
-                "percent": memory.percent
-            },
-            "storage": {
-                "total": storage.total,
-                "used": storage.used,
-                "free": storage.free,
-                "percent": storage.percent
-            }
-        },
-        "by_dir": get_storage(),
-        "total_cpu": 0,
-        "total_mem": 0
-    }
+            "by_dir": get_storage(),
+            "total_cpu": total_cpu,
+            "total_mem": total_mem,
+        }
 
-    for process in psutil.process_iter():
-        stats["by_pid"].append({
-            "pid": process.pid,
-            "name": process.name(),
-            "cpu": process.cpu_percent(interval=0.1),
-            "memory": process.memory_full_info().uss,
-            "shared": process.memory_full_info().shared,
-            "status": f"{process.status()} ({status_emojis.get(process.status())})"
-        })
-        stats["total_cpu"] += process.cpu_percent()
-        stats["total_mem"] += process.memory_info().rss
+        for process in psutil.process_iter():
+            stats["by_pid"].append(
+                {
+                    "pid": process.pid,
+                    "name": process.name(),
+                    "cpu": process.cpu_percent(interval=0.1),
+                    "memory": process.memory_full_info().uss,
+                    "shared": process.memory_full_info().shared,
+                    "status": f"{process.status()} ({status_emojis.get(process.status())})",
+                }
+            )
+            total_cpu += process.cpu_percent()
+            total_mem += process.memory_info().rss
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        # Skip processes that are gone/no access
+        pass
 
     return stats
 
 
-app.run(host='localhost', port=int(os.environ['PORT_RESOURCE_MONITOR']), debug=False)
+def main():
+    if check_redis_connection():
+        app.run(
+            host="localhost", port=int(os.environ["PORT_RESOURCE_MONITOR"]), debug=False
+        )
+
+
+if __name__ == "__main__":
+    main()
